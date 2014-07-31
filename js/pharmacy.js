35ed87624cee7b77eddc2ecee6cd24f22c6836fc
Sameer Manek <sameermanek@sameers-mbp.attlocal.net>
2014-07-31 19:59:59 -0400

    Adjusted width
    changed to remain in columns until 768px

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -1,5 +1,5 @@
 resizeMap=function(width){
-  if (width <= 991) { //using the equiv. of bootstrap medium width. Should be invoked for phones and tablets
+  if (width < 768) { //using the equiv. of bootstrap medium width. Should be invoked for phones and tablets
     $("#googleMapImage").attr("src", "http://maps.googleapis.com/maps/api/staticmap?size=360x240&scale=2&zoom=13&markers=icon:&markers=icon:http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=medical%257CFD7B70%7C475%20N%20Farnsworth%20Ave,%20Aurora,%20IL")
   } else {
     $("#googleMapImage").attr("src", "http://maps.googleapis.com/maps/api/staticmap?size=640x360&scale=2&zoom=13&markers=icon:&markers=icon:http://chart.apis.google.com/chart?chst=d_map_pin_icon%26chld=medical%257CFD7B70%7C475%20N%20Farnsworth%20Ave,%20Aurora,%20IL")
