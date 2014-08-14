f6975809f7912969f0fe6b94bf11bf7fa67aa876
Sameer Manek <sameermanek@sameers-mbp.attlocal.net>
2014-08-14 19:22:54 -0400

    test

--- header.php
+++ header.php
@@ -47,16 +47,18 @@
   </head>
 
   <body>
-
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="../">Aurora Pharmacy</a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li <?php if($_SERVER['PHP_SELF'] == '/contact.php') echo 'class="active"'?> ><a href="../contact.php">Contact Us</a></li>
-            </ul>
-        </div>
+    <div class="navbar navbar-default">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="../">Aurora Pharmacy</a>
+      </div>
+      <div class="navbar-collapse collapse navbar-responsive-collapse">
+        <ul class="nav navbar-nav">
+          <li <?php if($_SERVER['PHP_SELF'] == '/contact.php') echo 'class="active"'?> ><a href="../contact.php">Contact Us</a></li>
+        </ul>
       </div>
     </div>
